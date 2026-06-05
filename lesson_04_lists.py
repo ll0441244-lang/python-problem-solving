b65c2e1cf9a0ddf5d081b692b87b11100e116ea8
ll0441244-lang <ll0441244@gmail.com>
2026-06-05 16:50:17 +0300

    Create lesson_04_lists.py

---
+++ lesson_04_lists.py
@@ -0,0 +1,8 @@
+# وضع قائمه لتخزين المتغيرات دخلها
+my_services = ["1- Web Scraping","2- Automation","3- Data Analysis"]
+print ("----------my services ----------")
+#طبع المتغير باسمها مع طلب المتغيرات       
+print (my_services[0])
+print (my_services[1])
+print(my_services[2])
+print ("----------------------------------")
