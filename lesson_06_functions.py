f3162a33d75485abed564c80fc572fd2aabfd3cd
ll0441244-lang <ll0441244@gmail.com>
2026-06-05 17:25:59 +0300

    Create lesson_06_functions.py

---
+++ lesson_06_functions.py
@@ -0,0 +1,6 @@
+def calculate_days(articles_count):
+    days = int(articles_count / 2) 
+    # نفترض أنه يكتب مقالتين في اليوم
+    print("التحسين الذكي: الأيام المتوقعة لإنجاز مقالاتك هي:", days)
+calculate_days(20)
+# calculate_days هنا نطلب من بايثون ان المتغير يكون20
